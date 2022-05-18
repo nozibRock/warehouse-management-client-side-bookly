600ea90482325553025f5840a4c921e2ead0140b
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-05-18 12:31:06 +0600

    setup tailwind css with create react app

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2 className='text-4xl'>Save me from myself</h2>
+            <p>By give me death.</p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="">home</a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
