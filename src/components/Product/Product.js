3549f9ac8383e8719315f7b876969b05c0158af7
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-05-22 10:44:02 +0600

    Make products Responsive using tailwind css

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 const Product = ({product}) => {
-    const {id, name/* , author, publisher, isbn, pages, quantity, price,  */,img} = product;
+    const { name, author, price,  img} = product;
     return (
-      <div>
-          <img src={img} alt="" />
-        <h2> {id}. This is Product: {name}  </h2>
+      <div className="w-full md:w-1/2 xl:w-1/3 px-3  transition-all">
+          <div className='bg-black rounded-lg overflow-hidden mb-10'>
+            <img src={img} alt={name} className="w-full max-h-96 h-full hover:scale-110 transition-all object-cover" />
+            <div className='p-8 sm:p-9 md:p-7 xl:p-9'>
+                <h2 className='font-semibold text-white text-xl sm:text-[22px] md:text-xl 
+                                lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4 block'> {name}</h2>
+                <hr className='w-2/3 mx-auto mb-3' />
+                <div className='flex justify-center items-center md:flex-row flex-col'>
+                    <div className='w-full md:pl:16'>
+                        <h3 className="font-medium text-white text-lg mb-4">Author: {author} </h3>
+                         <p className="font-medium text-white text-lg mb-4"> Price:
+                            <span className='text-green-400'> TK.{price} </span> 
+                        </p>
+                    </div>
+                </div>
+                <div className='flex'>
+                    <button className='py-3 px-10 w-full lg:px-8 xl:px-10 inline-flex items-center justify-center text-center text-slate-100 font-semibold text-base bg-green-900 rounded-lg hover:bg-opacity-90 mx-2'>Manage</button>
+                </div>
+            </div>
+          </div>
       </div>
     );
 };
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -12,7 +12,7 @@ const Products = () => {
     
     return (
       <div>
-        <section className="bg-purple-500 lg:pt-[120px] pb-12 lg:pb-[90px] px-4 lg:px-0">
+        <section className="bg-white p-8 shadow-md shadow-[#9c9c9c] rounded lg:pt-[100px] pb-12 lg:pb-[80px] px-4 lg:px-0">
           <div className="container mx-auto">
             <div className="flex flex-wrap -mx-4">
               <div className="w-full px-4">
@@ -21,7 +21,7 @@ const Products = () => {
                   <span className="font-semibold text-lg text-sky-800 mb-2 block">
                     Recently Sold Products
                   </span>
-                  <h2 className=" font-bold text-3xl sm:text-4xl md:text-[40px] text-white mb-4">HOT SALE! 🔥</h2>
+                  <h2 className=" font-bold text-3xl sm:text-4xl md:text-[40px] text-black mb-4">HOT SALE! 🔥</h2>
                 </div>
               </div>
             </div>
@@ -32,6 +32,9 @@ const Products = () => {
                     <Product key={product.id} product={product}></Product>
                 )))
                 }
+            </div>
+            <div className='flex items-center justify-center'>
+                <button className='w-2/3 mx-auto inline-flex items-center justify-center h-12 px-6 font-medium text-gray-100 transition duration-200 rounded shadow-md bg-green-900 border-0 hover:bg-opacity-75 focus:shadow-outline focus:outline-none active:scale-90 text-lg md:text-xl md:tracking-wide'>Manage Products</button>
             </div>
           </div>
         </section>
