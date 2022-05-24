fd5068f7e05db65f18fbe63305e24102ba6b738c
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-05-24 09:25:16 +0600

    add navbar

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Banner from '../../components/Banner/Banner';
 import Domestic from '../../components/Domestic/Domestic';
 import Products from '../../components/Products/Products';
 import Services from '../../components/Sevices/Services';
@@ -7,7 +6,6 @@ import Services from '../../components/Sevices/Services';
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
             <Products></Products>
             <Domestic></Domestic>
             <Services></Services>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Navbar from '../Navbar/Navbar';
 
 const Header = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <h2 className='text-4xl'>Save me from myself</h2>
-            <p>By give me death.</p>
+            {/* <h2 className='text-4xl'>Save me from myself</h2>
+            <p>By give me death.</p> */}
+            <Banner></Banner>
         </div>
     );
 };
