27a518c61afe36398f32db73dbbb8e76dc305b16
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-06-02 01:02:57 +0600

    add navigate and signout

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -4,10 +4,13 @@ import auth from "../../firebase.init";
 import axios from "axios";
 import MyItem from "./MyItem/MyItem";
 import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
 
 const MyItems = () => {
   const [products, setProducts] = useState([]);
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
   useEffect(() => {
     async function getProducts() {
       const url = `http://localhost:5000/productlist?email=${user.email}`;
@@ -20,6 +23,8 @@ const MyItems = () => {
         setProducts(data);
       } catch (err) {
         if (err.response.status === 401 || err.response.status === 403) {
+          signOut(auth);
+          navigate("/signIn");
           localStorage.removeItem("accessToken");
           console.log("Error :()");
         }
